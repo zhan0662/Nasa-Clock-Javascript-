c1fd1b4e4352bc4db05ae7a7e51668e01ba0b726
Jimmy <zhan0662@algonquinlive.com>
2021-04-22 23:48:02 +0800

    Completes assignment

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ const $weekyear  = document.getElementById('weekyear')
 const now = new Date()
  const $settingform = document.getElementById('settingform')
 const $container  = document.getElementById('container')
-let $24h = ``
+let $seconds = ``
+let $date = ``
 let currenttime = ``
+let currentdate = ``
+
+if (localStorage.getItem('seconds')){
+  $seconds = localStorage.getItem('seconds')
+  $date = localStorage.getItem('date')
+}
 
 Date.prototype.getWeek = function() {
   let onejan = new Date(this.getFullYear(),0,1);
@@ -40,22 +47,36 @@ set.addEventListener("click", function() {
 
 
 setInterval(function time()  {
-  const now = new Date().toLocaleString();
+  const now = new Date()
   document.getElementById('now').innerHTML = now;
-   console.log($24h)
-   if($24h == "true"){
-  currenttime = Math.floor(now.getTime() / 1000 / 60 / 60 )
+  
+  console.log("seconds",$seconds)
+ if($seconds == "true"){
+  currenttime = now.toLocaleString().substring(10) 
   //currenttime = now.substring(9)
 }
 else{
-  currenttime = now.substring(9)
+
+
+    currenttime = `${now.getHours()}:${now.getMinutes()}`
+
+
+
+  }
+
+if($date == "true"){
+  currentdate = now.toLocaleString().substring(0,9)
+  //currenttime = now.substring(9)
+}
+else{
+    currentdate = ''
 }
 
   
   // console.log(currenttime)
   $time = document.getElementById('now')
-  $time.textContent = currenttime
-},1000)
+  $time.textContent =currentdate  + " "+ currenttime
+},100)
 
 fetch('https://api.nasa.gov/planetary/apod?api_key=mcaKjx5hAZ49i6QBGKdeXdaAFth59OlWfraB4Xxr')
       .then(function(response){
@@ -66,8 +87,8 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=mcaKjx5hAZ49i6QBGKdeXdaAFth59
             console.log(imageData)
             // if they are displaying a video instead of image.
             if (imageData.media_type === 'video') {
+              document.body.style.backgroundImage = "url('https://miro.medium.com/max/1800/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg')";
                 document.querySelector('h4').textContent = 'photo of the day is video'
-                document.body.style.backgroundImage =`url(https://api.nasa.gov/planetary/apod?api_key=mcaKjx5hAZ49i6QBGKdeXdaAFth59OlWfraB4Xxr&date=2021-04-10)`
             } else {    
                document.body.style.backgroundImage =`url(${imageData.url})`
                 document.querySelector('p').textContent = imageData.explanation
@@ -84,13 +105,30 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=mcaKjx5hAZ49i6QBGKdeXdaAFth59
  
   $settingform.addEventListener('submit', function (e) {
   e.preventDefault()
-
+  setting.classList.toggle("setting")
   const elements = $settingform.elements
-  console.log(elements[0].checked)
-  if(elements[0].checked == "true") {
-             $24h = elements[0].checked
+  console.log(elements)
+  if(elements[0].checked == true) {
+    $seconds = "true"
   }
  else{
-  $24h = false
+  $seconds = "false"
  }
+
+ if(elements[2].checked == true) {
+  $date = "true"
+}
+else{
+  $date = "false"
+}
+
+
+localStorage.setItem('seconds', $seconds);  
+localStorage.setItem('date', $date);
+
 })
+
+console.log(now.getHours())
+
+// //////////////////////////////////////////local storage ////////////////////////////////////////////////////
+
